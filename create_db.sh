2285621e298b26afbd684bb49a9de9401275b61c
Felix Dietze <felix.dietze@rwth-aachen.de>
2021-05-24 09:42:30 +0200

    clean up script

--- create_db.sh
+++ create_db.sh
@@ -13,15 +13,6 @@ main() {
 
 import-dataset() {
 	pv "$DATASET" | tail -n +2 | sqlite3 "$SQLITEDB" --init import-dataset.sql
-	# pv "$OUT/title.ratings.tsv.gz" | gzip -d | sqlite3 "$SQLITEDB" --init import-imdb-ratings.sql
-    # cat import-movielens-tags.sql | sqlite3 "$SQLITEDB" -init <(echo)
-	# cat process-data.sql |
-	# 	sed "s#:TMDB_DATA_DB_FILE#$TMDB_DATA_DB_FILE#" |
-	# 	sed "s#:TMDB_ID_DB_FILE#$TMDB_ID_DB_FILE#" |
-	# 	sed "s#:IMDB_KEYWORD_DB_FILE#$IMDB_KEYWORD_DB_FILE#" |
-	# 	sed "s#:MIN_MOVIE_VOTES#$MIN_MOVIE_VOTES#" |
-	# 	sed "s#:MIN_TVSHOW_VOTES#$MIN_TVSHOW_VOTES#" |
-	# 	sqlite3 "$SQLITEDB" -init <(echo)
 	echo "" | sqlite3 "$SQLITEDB" --init optimize.sql
 }
 
