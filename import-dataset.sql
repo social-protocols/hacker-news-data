13e82779221f96d312b7ef58fc3740e8cb390de3
Felix Dietze <felix.dietze@rwth-aachen.de>
2021-05-24 09:41:40 +0200

    set indices

--- import-dataset.sql
+++ import-dataset.sql
@@ -25,15 +25,31 @@ CREATE TABLE dataset (
 );
 
 
--- update dataset set topRank = null where topRank = -1;
-create index tick_id_idx on dataset(tick, id);
-create index id_tick_idx on dataset(id, tick);
 
-create view gain as select id, score, tick, score-(lag(score) over (partition by id order by tick)) as gain from dataset;
+
 
 .mode csv
 .import /dev/stdin dataset
 
 .headers off
 
+
+
+-- TODO: id = -1
+-- update dataset set topRank = null where topRank = -1;
+
+SELECT "Creating indices...";
+create index tick_id_idx on dataset(tick, id);
+create index id_tick_idx on dataset(id, tick);
+create index id_age_idx on dataset(id,sampleTime-submissionTime);
+-- create index date_idx on dataset(date(sampleTime));
+
+-- SELECT "Calculating fullstories...";
+-- alter table dataset add column full bool not null default false;
+-- update dataset d set full = true where id in (select * from dataset where id in (select distinct id from dataset where (sampleTime-submissionTime) < 180));
+-- create table fullstories as ;
+
+-- SELECT "Calculating gain...";
+-- alter table dataset add column gain integer;
+-- create table gain as select * from(select *, (score-lag(score) over (partition by id order by tick)) as gain from dataset) where gain not null;
 
