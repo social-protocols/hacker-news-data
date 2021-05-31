79f2cc235a2c3f509714cd9b362111ea53be52c1
JohannesNakayama <johannes.nakayama@rwth-aachen.de>
2021-05-31 09:11:52 +0200

    calculate gain

--- import-dataset.sql
+++ import-dataset.sql
@@ -42,14 +42,14 @@ SELECT "Creating indices...";
 create index tick_id_idx on dataset(tick, id);
 create index id_tick_idx on dataset(id, tick);
 create index id_age_idx on dataset(id,sampleTime-submissionTime);
--- create index date_idx on dataset(date(sampleTime));
+create index date_idx on dataset(date(sampleTime, 'unixepoch'));
 
--- SELECT "Calculating fullstories...";
--- alter table dataset add column full bool not null default false;
--- update dataset d set full = true where id in (select * from dataset where id in (select distinct id from dataset where (sampleTime-submissionTime) < 180));
--- create table fullstories as ;
+delete from dataset where id = -1;
 
--- SELECT "Calculating gain...";
--- alter table dataset add column gain integer;
--- create table gain as select * from(select *, (score-lag(score) over (partition by id order by tick)) as gain from dataset) where gain not null;
+SELECT "Calculating fullstories...";
+create table fullstories as select distinct id from dataset where (sampleTime-submissionTime) < 180;
+
+SELECT "Calculating gain...";
+alter table dataset add column gain integer;
+update dataset as d set gain = (select gain from (select id, sampleTime, (score-lag(score) over (partition by id order by tick)) as gain from dataset) where id = d.id and sampleTime = d.sampleTime);
 
