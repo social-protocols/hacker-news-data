a481c7c33cd0de2bec52ff75fb95779a825f040f
Felix Dietze <felix.dietze@rwth-aachen.de>
2021-06-17 00:38:35 +0200

    improve fetch data, remove public_fetch_data

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
+set -Eeuo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/#:~:text=set%20%2Du,is%20often%20highly%20desirable%20behavior.
 
-osf init 
-osf fetch /data/processed/hacker-news-processed.7z ./hacker-news-processed.7z
-7z x ./hacker-news-processed.7z
+osf -p bnysw fetch -f /data/processed/hacker-news-processed.7z ./hacker-news-processed.7z
+7z x ./hacker-news-processed.7z -aoa
 rm hacker-news-processed.7z
-
--- fetch_data_public.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-osf fetch https://osf.io/bnysw/ ./hacker-news-processed.7z
-7z x ./hacker-news-processed.7z
-rm hacker-news-processed.7z
