f379e99c0ffcf22131374be6c3d4f6246065f53c
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-01-14 17:35:33 +0100

    A lot of housekeeping
    * some old stuff archived (not deleted)

--- data/retrieve_data.R
+++
@@ -1,8 +0,0 @@
-# To run this script: 
-# * generate a personal access token (PAT) on OSF
-# * create a file .Renviron in this directory and save the PAT as an
-#   environment variable (OSF_PAT = <YOUR TOKEN>)
-
-library(osfr)
-
-osf_proj <- osf_retrieve_node("bnysw")
