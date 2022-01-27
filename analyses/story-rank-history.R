daf1ecdb4e6e572ca61405f0a470dbd71f791985
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-01-28 00:42:27 +0100

    Cleaned up plot options
    * three options, comment in/out to switch

--- analyses/story-rank-history.R
+++ analyses/story-rank-history.R
@@ -8,11 +8,18 @@ library(lubridate)
 library(glue)
 library(httr)
 
+
 # Set locale
 Sys.setlocale("LC_ALL","en_US.UTF-8")
 
+
+# ID of sample story
+selected_id <- 29621574
+
+
 # Connect to database
 con <- RSQLite::dbConnect(RSQLite::SQLite(), "../data/hacker-news.sqlite")
+
 
 # # For reference: Table of stories scoring higher than or equals 100
 # highest_scoring_stories <- dbGetQuery(
@@ -29,21 +36,18 @@ con <- RSQLite::dbConnect(RSQLite::SQLite(), "../data/hacker-news.sqlite")
 # )
 
 # Get story from API
-selected_id <- 29621574
 url <- glue("https://hacker-news.firebaseio.com/v0/item/{selected_id}.json")
 story <- content(GET(url[1]))
+
 
 # Get rank history for representative story
 data <- dbGetQuery(
   con,
   "
   SELECT 
-    id, 
-    score, 
-    sampleTime,
+    id, score, sampleTime,
     CAST(sampleTime - submissionTime AS FLOAT) / (60 * 60) AS ageHours, 
-    topRank, 
-    gain
+    topRank, gain
   FROM dataset
   WHERE id={selected_id};
   " %>% glue()
@@ -56,20 +60,16 @@ data <- dbGetQuery(
   mutate(topRankBin = log2(topRank) + 1) %>% 
   replace_na(list(topRankBin = -1)) %>% 
   mutate(topRankBin = as.integer(topRankBin))
-  
 
-expected_votes <- dbGetQuery(
-  con,
-  "
-  SELECT *
-  FROM expectedUpvotes
-  " %>% glue()
-) %>% 
+  
+# Get expected upvotes
+expected_upvotes <- dbGetQuery(con, "SELECT * FROM expectedUpvotes" %>% glue()) %>% 
   mutate(topRankBin = as.integer(topRankBin))
 
 
+# Join expected upvotes to data
 data %<>% 
-  left_join(expected_votes, by = c("topRankBin", "timeofdayBin", "dayofweekBin"))
+  left_join(expected_upvotes, by = c("topRankBin", "timeofdayBin", "dayofweekBin"))
 
 
 # Wrangle data into plottable format
@@ -82,32 +82,32 @@ data %>%
   ) %>% 
   ungroup() %>% 
   mutate(topRank = -topRank) %>% 
-  filter(ageHours <= 24) -> line_data
+  filter(ageHours <= 24) -> rank_history
 
-bar_data <- line_data %>% 
+
+# Gain data for plot
+gain_data <- rank_history %>% 
   select(ageHoursFloor, actual, expected) %>% 
   distinct() %>% 
   pivot_longer(cols = c(actual, expected), names_to = "metric", values_to = "value") %>% 
   mutate(metric = factor(metric))
 
 
+# Theme element that are the same for all options
+base_theme <- theme(
+  plot.caption = element_text(hjust = 0),
+  legend.title = element_blank(),
+  text = element_text(family = "Courier"),
+  axis.title.x = element_text(margin = margin(t = 10, b = 20)),
+  panel.background = element_rect(fill = "#f6f6ef"),
+  panel.grid.major = element_line(color = "grey90"),
+  panel.grid.minor = element_blank(),
+  axis.line = element_line(color = "black")
+)
+
 # Plot
-ggplot(line_data) +
+p <- ggplot(rank_history) +
   geom_line(aes(x = ageHours, y = topRank + 90), color = "#ff6600") +
-  # only upvotes in hour
-  # geom_bar(
-  #   data = plot_data %>% select(ageHoursFloor, sumGainInHour) %>% distinct(),
-  #   aes(x = ageHoursFloor, y = sumGainInHour), stat = "identity", fill = "black"
-  # ) +
-  # upvotes and expected upvotes
-  geom_bar(
-    data = bar_data,
-    aes(x = ageHoursFloor, y = value, fill = metric),
-    stat = "identity",
-    position = "dodge",
-    width = 1
-  ) +
-  scale_fill_manual(values = c("actual" = "black", "expected" = "gold3")) +
   scale_x_continuous(breaks = seq(0, 24, 4)) +
   scale_y_continuous(
     breaks = seq(0, 85, 5),
@@ -125,26 +125,61 @@ ggplot(line_data) +
       paste0("Orange line indicates the rank history, black bars the gained votes per hour.")
     )
   ) +
-  theme(
-    plot.caption = element_text(hjust = 0),
-    legend.title = element_blank(),
-    # no legend if not expected upvotes
-    # legend.position = "None",
-    # otherwise
-    legend.position = c(0.9, 0.9),
-    legend.box.background = element_rect(fill = "white", color = "black"),
-    text = element_text(family = "Courier"),
-    axis.title.x = element_text(margin = margin(t = 10, b = 20)),
-    panel.background = element_rect(fill = "#f6f6ef"),
-    panel.grid.major = element_line(color = "grey90"),
-    panel.grid.minor = element_blank(),
-    axis.line = element_line(color = "black")
+  base_theme
+
+
+# Only upvotes in hour
+p_a <- p +
+  geom_bar(
+    data = rank_history %>% select(ageHoursFloor, actual) %>% distinct(),
+    aes(x = ageHoursFloor, y = actual), stat = "identity", fill = "black"
+  )
+
+# Upvotes and expected upvotes
+p_b <- p +
+  geom_bar(
+    data = gain_data,
+    aes(x = ageHoursFloor, y = value, fill = metric),
+    stat = "identity",
+    position = "dodge",
+    width = 1
   ) +
-  NULL
+  scale_fill_manual(values = c("actual" = "black", "expected" = "gold3"))
+
+p_c <- p +
+  geom_line(
+    data = gain_data,
+    aes(x = ageHoursFloor, y = value, color = metric)
+  ) +
+  geom_point(
+    data = gain_data,
+    shape = 21,
+    fill = "white",
+    size = 2,
+    aes(x = ageHoursFloor, y = value, color = metric)
+  ) +
+  scale_color_manual(values = c("actual" = "black", "expected" = "gold3"))
+
+
+# Final plot
+# final <- p_a + theme(legend.position = "None")
+
+# final <- p_b +
+#   theme(
+#     legend.position = c(0.9, 0.9),
+#     legend.box.background = element_rect(fill = "white", color = "black")
+#   )
+
+final <- p_c +
+  theme(
+    legend.position = c(0.9, 0.9),
+    legend.box.background = element_rect(fill = "white", color = "black")
+  )
+
 
 # Save plot
-ggsave("plots/story-rank-history.png", width = 8, height = 6)
-ggsave("plots/story-rank-history.svg", width = 8, height = 6)
+ggsave(plot = final, "plots/story-rank-history.png", width = 8, height = 6)
+ggsave(plot = final, "plots/story-rank-history.svg", width = 8, height = 6)
 
 # Disconnect from database
 RSQLite::dbDisconnect(conn = con)
