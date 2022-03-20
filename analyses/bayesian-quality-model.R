b0cd3ca904c0f67effbdde340a5d105ea0ece705
jwarden <jwarden@ziprecruiter.com>
2022-03-20 22:46:23 +0100

    add Bayesian hierarchial model

---
+++ analyses/bayesian-quality-model.R
@@ -0,0 +1,59 @@
+# Connect to the sqlite database
+library(DBI)
+con <- RSQLite::dbConnect(RSQLite::SQLite(), "../data/hacker-news.sqlite")
+
+# A library accompanying the Richard McElreath's "Statistical Rethinking",
+# with a bunch of utility methods, wrappers around STAN.
+library(rethinking)
+
+# Grab actual/expected upvotes from a random sample of stories. We are
+# defining quality as the ratio of actual/expected upvotes (at that
+# rank/time). But of course if there is a small amount of upvotes, the actual
+# ratio might not represent the 'true' quality, or long-term average ratio for
+# that story. A hierarchical model can help us make better estimates of true
+# quality and differentiate the effect of quality from random noise.
+
+upvotesVSExpected <- dbGetQuery(con, "
+    SELECT
+        id,
+        sid,
+        upvotes,
+        row_number() over () as sampleNumber,
+        max(total_upvotes) as totalUpvotesForStory,
+        expectedUpvotes,
+        cumulativeQuality as quality
+    FROM 
+        random_sample_100_stories
+        JOIN quality USING (id)
+        GROUP BY id, sid
+")
+
+
+# Pretty simple Bayesian hierarchical model.
+model <- ulam(
+    alist(
+        upvotes ~ dpois(lambda),
+
+        # The rate that votes arrive is equal to the expected upvotes (at this time/rank)
+        # times quality. The formula below is quality*avg_quality*expectedUpvotes. We include
+        # both a story-specific quality (indexed by sid) and an overall average quality across
+        # all stories, which should be very close to 1 by definition (so log quality should be 
+        # close to zero).
+        lambda <- exp(log_quality[sid] + avg_log_quality)*expectedUpvotes,
+
+        # Key to the model. Each story (indexed by sid) has a log quality
+        # drawn from a normal distribution. This means for example that a
+        # story getting 2x as many votes as expected is as common as one
+        # getting 1/2x as many (ln 2 = - ln 1/2 = .693).
+        log_quality[sid] ~ dnorm(0, sigma_quality),
+
+        # Super weak prior on the standard deviation of quality.
+        sigma_quality ~ dunif(0, 5),
+
+        # Average quality should be close to zero, but make this a parameter
+        # not a constant to see if the data fits the model as expected.        
+        avg_log_quality ~ dnorm(0, 5)
+    ), data=upvotesVSExpected
+)
+precis(model, depth=2)
+
