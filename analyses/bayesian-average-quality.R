648b76a86b96d87f9850e6197d46666995b6874f
Jonathan R. Warden <john.warden@gmail.com>
2022-05-23 19:53:02 +0200

    clean up comments in bayesian-average-quality.R

--- analyses/bayesian-average-quality.R
+++ analyses/bayesian-average-quality.R
@@ -1,61 +1,46 @@
-# Okay the idea here is that the estimates for quality that we got using MCMC in bayesian-quality-model.R can be
-# Estimated using a very simple "Bayesian Averaging" formula. S
+library(ggplot2)
+library(glue)
 
-# First, run the Bayesian quality model
+# Okay the idea here is that the estimates for quality that we got using a
+# Bayesian model and MCMC in bayesian-quality-model.R can be approximated
+# using a very simple "Bayesian Averaging" formula.
+
+
+# First, run the Bayesian quality model.
 source("bayesian-quality-model.R")
 
-# Pull the average posterior estimate for log_quality for all n stories
-# And add to the data frame
-samples$posteriorLogQuality = coef(model)[1:n]
+# Pull the average posterior estimate for log_quality for all n stories,
+# and add to the data frame.
+stories$posteriorLogQuality = coef(model)[1:n]
 
 
 # Now chart. This chart shows that there is a strong linear relationship
-# between the log of the quality ratio and the bayesian estiate of quality
+# between the log of the quality ratio and the Bayesian estimate of quality
 # when there are a lot of upvotes (a lot of evidence). That is, a simple
 # ratio of upvotes/expectedUpvotes is a good estimate of quality when there
-# is lots of evidence. But when there are fewer upvotes a simple ratio
+# is lots of evidence, but when there are fewer upvotes a simple ratio
 # greatly over-estimates quality.
-ggplot(samples, aes(x = posteriorLogQuality, y = log(qualityRatio))) + geom_point(aes(size = upvotes))
+ggplot(stories, aes(x = posteriorLogQuality, y = log(qualityRatio))) + geom_point(aes(size = upvotes))
 ggsave(file = "plots/quality ratio vs MCMC log scale.png"
 , height = 5, width = 5)
 
 
-# Same chart, but using linear instead of logarithmic scale.
-# ggplot(samples, aes(x = exp(posteriorLogQuality), y = qualityRatio)) + geom_point(aes(size = upvotes))
+## Bayesian Average of Log Quality. This is a weighted average between the log
+## quality ratio and zero (the prior expectation of the average log quality
+## ratio). The weights are the number of upvotes and some constant value,
+## respectively. The constant is chosen based on whatever works
+## (whatever makes the bayesian-average estimate closest to the MCMC
+## estimate), but it can be interpreted as representing the strength of the
+## prior. I found the constant value below by manually running a linear
+## regression with various other values.
 
-# constant = rep(10, times=n)
-# samples$bayesianAverageQuality = (samples$qualityRatio*samples$upvotes + constant) / (samples$upvotes + constant)
-# ggplot(samples, aes(x = exp(posteriorLogQuality), y = bayesianAverageQuality)) + geom_point(aes(size = upvotes))
+constant = 9
+stories$bayesianAverageLogQuality = (log(stories$qualityRatio)*stories$upvotes) / (stories$upvotes + constant)
 
-# linearMod <- lm(exp(posteriorLogQuality) ~ bayesianAverageQuality, data=samples)
-# summary(linearMod)
+ggplot(stories, aes(x = posteriorLogQuality, y = bayesianAverageLogQuality)) + geom_point(aes(size = upvotes+1))
+ggsave(file = glue("plots/bayesian average vs MCMC log scale (constant={constant}).png"), height = 5, width = 5)
 
-# 8: .8376
-# 10: .8714
-# 12: .89
-# 14: .8995
-# 15: 0.9019,
-# 17: 0.9037
-# 18: 0.9036
-# 20:  0.9013
-# 25: .8903
-
-
-## Bayesian Average of Log Quality
-constant = rep(3, times=n)
-samples$bayesianAverageLogQuality = (log(samples$qualityRatio)*samples$upvotes) / (samples$upvotes + constant)
-ggplot(samples, aes(x = posteriorLogQuality, y = bayesianAverageLogQuality)) + geom_point(aes(size = upvotes))
-ggsave(file = "plots/bayesian average vs MCMC log scale (constant=3).png", height = 5, width = 5)
-
-
-linearMod <- lm(posteriorLogQuality ~ bayesianAverageLogQuality, data=samples)
+linearMod <- lm(posteriorLogQuality ~ bayesianAverageLogQuality, data=stories)
 summary(linearMod)
 
-# 4: .9902
-# 3.5: .992
-# 3.25: .9925
-# 3.125: 9925
-# 3: .9927
-# 2.875: .9925
-# 2.75: 9923
-# 2: .9869
+
