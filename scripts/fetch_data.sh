145c6adb72da319a54098f784ad7bee7d475e87f
JohannesNakayama <johannes.nakayama@rwth-aachen.de>
2022-11-22 19:09:37 +0100

    Remove osfclient zombie code

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -4,7 +4,6 @@ set -Eeuo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_
 echo "Fetching data from osf..."
 
 mkdir -p data
-# pipenv run osf -p bnysw fetch -f /data-v1/hacker-news.7z data/hacker-news.7z
 wget --no-verbose --show-progress https://osf.io/h9sjy/download -O data/hacker-news.7z
 7z x data/hacker-news.7z -aoa -odata
 rm data/hacker-news.7z
