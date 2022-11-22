41cdad37df7f10d2d37538073d3423fdd5bab5de
JohannesNakayama <johannes.nakayama@rwth-aachen.de>
2022-11-22 19:08:58 +0100

    Start setup script for first time use
    so far:
    - download data and create database
    - setup .Renviron

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-
-./scripts/fetch_data.sh
-./scripts/create_db.sh data/newstories_2021-11-23_22-09-04.tsv
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+
+# ./scripts/fetch_data.sh
+# ./scripts/create_db.sh data/newstories_2021-11-23_22-09-04.tsv
+
+ROOTDIR=$(pwd)
+echo "PROJDIR=${ROOTDIR}" >> ./analyses/.Renviron
